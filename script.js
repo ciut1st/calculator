4ecd364dac972417ce42aacf940cf4b75e251dc2
ciut1st <williamyeoshuhok1st@gmail.com>
2023-03-10 16:19:04 +1100

    basic preparation

---
+++ script.js
@@ -0,0 +1,19 @@
+// Addition
+function addition(a, b) {
+    return a + b
+}
+
+// Substraction
+function substraction(a, b) {
+    return a -b
+}
+
+// Multiplication
+function multiplication(a, b) {
+    return a * b
+}
+
+// Division
+function division(a, b) {
+    return a / b
+}

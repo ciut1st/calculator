560b244f9a80ccf6c90fe8f4fa0c8d16e8e3af98
ciut1st <williamyeoshuhok1st@gmail.com>
2023-03-10 17:59:03 +1100

    fix typo

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function addition(a, b) {
 
 // Substraction
 function substraction(a, b) {
-    return a -b
+    return a - b
 }
 
 // Multiplication
@@ -17,3 +17,24 @@ function multiplication(a, b) {
 function division(a, b) {
     return a / b
 }
+
+
+let a = parseFloat(prompt("Pick a number: "))
+    let b = parseFloat(prompt("Pick another number: "))
+    let operator = prompt("Pick an operator: ")
+// Operation
+function operate(a, operator, b) {
+    if (operator === '+') {
+        return addition(a, b)
+    } else if (operator === '-') {
+        return substraction(a, b)
+    } else if (operator === '*') {
+        return multiplication(a, b)
+    } else if (operator === '/') {
+        return division(a, b)
+    } else {
+        return "Error"
+    }
+}
+
+console.log(operate(a, operator, b))
